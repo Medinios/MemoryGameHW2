9e22b893ace6c03d9692d7939140f29605c24358
Sam Medina <smedina@fortinet.com>
2021-11-29 11:33:31 +0200

    small changes

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 let userData = {
     username: '',
     username2: '',
-    points: 0,
-    gameSize: 4,
+    pointsUserA: 0,
+    pointsUserB: 0,
+    gameSize: '4x4',
+}
+
+let currentPlayer = "playerA";
+
+let gameHistory = {
+    history: []
 }
 
 const POINT_PER_WIN = 1;
 
 shuffleCards = () => {
+    const cardsNamesList = ['card.png'];
+    //createClass - inside for boardSize if 5x5 == 25;
+    //appendClass
+    // <div class="card" id=`${cardName}`>
+    //                     <div class="card__front">
+    //                         <img src="images/card1.png">
+    //                     </div>
+    //                     <div class="card__back">
+    //                         <img src="images/back.png">
+    //                     </div>
+    //                 </div>
     const cards = document.querySelectorAll('.card');
     const randList = [];
     Array.from(cards).forEach((card) => {
@@ -19,7 +37,9 @@ shuffleCards = () => {
         setTimeout(()=> {
             card.style.order = rand;
         }, 400);
+        card.addEventListener('click', flip.bind(null, card));
     });
+
 }
 
 checkWinner = ([card1, card2]) => {
@@ -30,10 +50,33 @@ checkWinner = ([card1, card2]) => {
     }
     const matchedCards = Array.from(document.querySelectorAll('.card')).filter(card => card.classList.contains('matched'));
     const cards = document.querySelectorAll('.card');
+    //check if end game. (all the cards flipped)
     if(matchedCards.length == cards.length) {
+        if(userData.pointsUserA > userData.pointsUserB) {
+            setTimeout(()=> { alert(`winner ${userData.username}`); }, 1000);
+        } else if(userData.pointsUserA < userData.pointsUserB) {
+            setTimeout(()=> { alert(`winner ${userData.username2}`); }, 1000);
+        } else {
+            setTimeout(()=> { alert('winner TIE'); }, 1000);
+        }
+        createNewGame();
         //end game
-        setTimeout(()=> { alert('winner'); }, 1000);
+
     }
+}
+
+
+//inital game right after win.
+createNewGame = () => {
+    gameHistory.history.push({date:Date.now(),...userData});
+    userData = {
+        username: '',
+        username2: '',
+        pointsUserA: 0,
+        pointsUserB: 0,
+        gameSize: '4x4',
+    };
+    document.getElementsByClassName("userData")[0].style.display = 'block';
 }
 
 flip = (card) => {
@@ -42,28 +85,33 @@ flip = (card) => {
     if (cards.length === 2) {
         changePoints(POINT_PER_WIN);
         checkWinner(cards);
+        changeCurrentPlayer();
     }
+}
 
+changeCurrentPlayer = () => {
+    currentPlayer = currentPlayer == "PlayerB" ? "PlayerA" : "PlayerB";
+    const playerUser = currentPlayer == "PlayerB" ? userData.username2 : userData.username;
+    document.getElementsByClassName(`currentPlayer`)[0].innerHTML = playerUser;
 }
 
 changePoints = (points) => {
-    let point = document.getElementsByClassName('pointsCounter')[0].innerHTML;
+    let point = document.getElementsByClassName(`pointsCounter${currentPlayer}`)[0].innerHTML;
     const newPoints =parseInt(point) +  points;
-    document.getElementsByClassName('pointsCounter')[0].innerHTML = newPoints;
+    document.getElementsByClassName(`pointsCounter${currentPlayer}`)[0].innerHTML = newPoints;
 }
 
 setUserData = () => { 
     const username = document.getElementsByName("name")[0].value;
     const username2 = document.getElementsByName("name2")[0].value;
+    const gameSize = document.getElementsByName("board")[0].value;
     userData['username'] = username;
     userData['username2'] = username2;
-    userData['gameSize'] = 4;
+    userData['gameSize'] = gameSize;
+    document.getElementsByClassName("userData")[0].style.display = 'none';
+    changeCurrentPlayer();
     shuffleCards();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const cards = document.querySelectorAll('.card');
-    Array.from(cards).map((card) => {
-        card.addEventListener('click', flip.bind(null, card));
-    })
 });
